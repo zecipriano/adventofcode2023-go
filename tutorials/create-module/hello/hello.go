ea0ef8ef082ccfb8d1915428e8bc952f5b3e7644
Zé Cipriano <ze@zecipriano.com>
2023-11-05 12:02:46 +0000

    Greetings multiple people

--- tutorials/create-module/hello/hello.go
+++ tutorials/create-module/hello/hello.go
@@ -14,16 +14,17 @@ func main() {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 
-	// Request a greeting message.
-	message, err := greetings.Hello("Gladys")
+	// A slice of names.
+	names := []string{"Gladys", "Samantha", "Darrin"}
 
-	// If an error was returned, print it to the console and
-	// exit the program.
+	// Request greeting messages for the names.
+	messages, err := greetings.Hellos(names)
+
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// If no error was returned, print the returned message
-	// to the console.
-	fmt.Println(message)
+	// If no error was returned, print the returned map of
+	// messages to the console.
+	fmt.Println(messages)
 }
